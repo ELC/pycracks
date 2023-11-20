9074748fbc27decc131f7ac41186b7ed820678ac
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2023-11-20 19:25:47 -0300

    fix(cicd): improve pylint configuration

--- pycracks/logger.py
+++ pycracks/logger.py
@@ -4,10 +4,10 @@ logger = logging.getLogger(__name__)
 
 handler = logging.StreamHandler()
 
-format_string = (
+FORMAT_STRING = (
     "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s"
 )
-formatter = logging.Formatter(format_string)
+formatter = logging.Formatter(FORMAT_STRING)
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
